4c7071e95087376c9ba9c351efc074f31f6f20d0
root <root@localhost.localdomain>
2026-06-10 22:54:47 +0000

    My Ludo App First Version

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(child: Text("লুডু খেলা এইচডি")),
+      ),
+    ));
+
