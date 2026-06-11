e23f57fe224d6dd30c07381adf790b5dfa2de2a4
Roykk55 <mxshohan555@gmail.com>
2026-06-11 03:49:52 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(child: Text("লুডু খেলা এইচডি")),
+void main() => runApp(LudoApp());
+
+class LudoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LudoHomeScreen(),
+    );
+  }
+}
+
+class LudoHomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("এলা লুডু ক্লোন")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("লুডু খেলা এইচডি", style: TextStyle(fontSize: 24)),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {}, 
+              child: Text("২ জন খেলুন")
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {}, 
+              child: Text("৪ জন খেলুন")
+            ),
+          ],
+        ),
       ),
-    ));
+    );
+  }
+}
 
