34669e4e4fa04b50f0cdf55880f62183666f7780
Roykk55 <mxshohan555@gmail.com>
2026-06-11 12:26:41 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,56 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(debugShowCheckedModeBanner: false, home: LudoBoardFinal()));
+void main() => runApp(MaterialApp(debugShowCheckedModeBanner: false, home: LudoGameBoard()));
 
-class LudoBoardFinal extends StatefulWidget {
+class LudoGameBoard extends StatefulWidget {
   @override
-  _LudoBoardFinalState createState() => _LudoBoardFinalState();
+  _LudoGameBoardState createState() => _LudoGameBoardState();
 }
 
-class _LudoBoardFinalState extends State<LudoBoardFinal> {
-  // গুটির পজিশন - ৪টি রঙের জন্য ১৬টি গুটি
-  List<Offset> positions = List.generate(16, (i) => Offset(20, 20));
+class _LudoGameBoardState extends State<LudoGameBoard> {
+  // ১৬টি গুটির পজিশন লিস্ট - ৪টি রঙের জন্য ১৬টি গুটি
+  List<Offset> coinPositions = List.generate(16, (i) {
+    // প্রতি ৪টি গুটিকে বোর্ডের আলাদা আলাদা কর্নারে সাজানোর লজিক
+    double x = (i % 4) * 40.0 + 20.0;
+    double y = (i ~/ 4) * 40.0 + 20.0;
+    return Offset(x, y);
+  });
+
+  void rollDice() {
+    setState(() {
+      int dice = (1 + DateTime.now().microsecond % 6);
+      // গুটিগুলো যেন এলোমেলোভাবে না নড়ে, বরং একটি নির্দিষ্ট দূরত্বে নড়ে
+      for (int i = 0; i < 16; i++) {
+        coinPositions[i] = Offset(
+          (coinPositions[i].dx + (dice * 5)) % 260,
+          (coinPositions[i].dy + (dice * 5)) % 260
+        );
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.black, // বোর্ডের বর্ডার ব্ল্যাক
       body: Center(
         child: Container(
-          width: 320, height: 320, color: Colors.white,
+          width: 300, height: 300, color: Colors.white,
           child: Stack(children: [
-            // ১. সুন্দর বোর্ড ডিজাইন (৪টি কালার জোন)
-            Positioned(top: 0, left: 0, child: Container(width: 160, height: 160, color: Colors.red.withOpacity(0.3))),
-            Positioned(top: 0, right: 0, child: Container(width: 160, height: 160, color: Colors.green.withOpacity(0.3))),
-            Positioned(bottom: 0, left: 0, child: Container(width: 160, height: 160, color: Colors.yellow.withOpacity(0.3))),
-            Positioned(bottom: 0, right: 0, child: Container(width: 160, height: 160, color: Colors.blue.withOpacity(0.3))),
-            
-            // ২. গ্রিড লাইন বা বর্ডার (ডিজাইন আরও প্রফেশনাল করার জন্য)
-            CustomPaint(painter: BoardPainter()),
-
-            // ৩. ১৬টি গুটি (এনিমেশনসহ)
-            ...List.generate(16, (i) => AnimatedPositioned(
-              duration: Duration(milliseconds: 500),
-              curve: Curves.easeInOut,
-              left: positions[i].dx,
-              top: positions[i].dy,
-              child: Icon(Icons.circle, color: (i < 4) ? Colors.red : (i < 8) ? Colors.green : (i < 12) ? Colors.yellow : Colors.blue, size: 24),
+            // ১৬টি গুটিকে স্ক্রিনে প্রদর্শন করা
+            ...List.generate(16, (i) => Positioned(
+              left: coinPositions[i].dx,
+              top: coinPositions[i].dy,
+              child: Icon(Icons.circle, color: (i < 4) ? Colors.red : (i < 8) ? Colors.green : (i < 12) ? Colors.yellow : Colors.blue, size: 25),
             )),
           ]),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            // এখানে ক্লিক করলে গুটিগুলো বোর্ডের নির্দিষ্ট রাউন্ড পথে ঘুরবে
-            positions = List.generate(16, (i) => Offset(40.0 + (i * 10) % 200, 40.0 + (i * 10) % 200));
-          });
-        },
-        child: Icon(Icons.play_arrow),
+        onPressed: rollDice,
+        child: Icon(Icons.casino),
+        backgroundColor: Colors.orange,
       ),
     );
   }
-}
-
-// বোর্ডের দাগগুলো আঁকার জন্য
-class BoardPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    Paint paint = Paint()..color = Colors.black..strokeWidth = 2;
-    canvas.drawLine(Offset(160, 0), Offset(160, 320), paint);
-    canvas.drawLine(Offset(0, 160), Offset(320, 160), paint);
-  }
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
